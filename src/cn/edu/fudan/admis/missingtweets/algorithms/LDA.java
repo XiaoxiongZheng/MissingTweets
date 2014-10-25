296044e18e0a0cc244d1b6e7b61b911e997402cd
XiaoxiongZheng <zhengxx012@gmail.com>
2014-10-25 20:41:39 +0800

    stop words

---
+++ src/cn/edu/fudan/admis/missingtweets/algorithms/LDA.java
@@ -0,0 +1,12 @@
+package cn.edu.fudan.admis.missingtweets.algorithms;
+
+public class LDA {
+	int[][] doc;			
+	int v, k, m;
+	int[][] z;
+	
+	// dirichlet prior parmeter
+	double alpha;
+	double beta;
+	
+}
